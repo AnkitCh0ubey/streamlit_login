6a9303b1f257cdb3ef45e0a92c76e103a4d3725b
unknown <ankitchoubey110@gmail.com>
2024-09-19 18:23:17 +0530

    auth patch 2

--- pages/login.py
+++ pages/login.py
@@ -1,4 +1,3 @@
-import bcrypt
 import streamlit as st
 from yaml import SafeLoader
 import credentials as cr
@@ -7,57 +6,6 @@ import streamlit_authenticator as stauth
 
 
 st.set_page_config(page_title="Login")
-# if 'authenticated' not in st.session_state:
-#     st.session_state['authenticated'] = False
-
-
-
-
-
-# def authenticate(mail, password):
-#     users = load_credentials()
-#     if mail in users and users[mail]:
-#         stored_password = users[mail]['password']
-#         if bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8')):
-#             return users[mail]['role']
-#     return None
-#
-#
-
-
-# def login():
-#     st.subheader("Login")
-#
-#     mail = st.text_input("Email Address")
-#     password = st.text_input("Password", type="password")
-#
-#     try:
-#         if st.button("Login"):
-#             role = authenticate(mail, password)
-#
-#             if role == "admin":
-#                 st.session_state['authenticated'] = True
-#                 st.session_state['role'] = role
-#                 st.success("Login successful! Welcome Admin, Redirecting to your workspace...")
-#                 cr.key = 2
-#                 st.write('<meta http-equiv="refresh" content="0; url=/admin_dashboard" />', unsafe_allow_html=True)
-#
-#             elif mail == "" or password == "":
-#                 st.error("Please enter both email and password.")
-#
-#             elif role == "user":
-#                 st.session_state['authenticated'] = True
-#                 st.session_state['role'] = role
-#                 st.success("Login successful! Redirecting to Dashboard...")
-#                 cr.key = 1
-#                 st.experimental_set_query_params(logged_in="true")
-#                 st.write('<meta http-equiv="refresh" content="0; url=/dashboard" />', unsafe_allow_html=True)
-#
-#             else:
-#                 st.error("ERROR: Invalid Credentials")
-#
-#     except Exception as e:
-#         st.error(f"An unexpected error occurred. Please try again. {e}")
 
 def load_credentials():
     with open('./config.yaml') as file:
